33d7e2f0d0da6e46c1a1ecac6a7b43410b661684
root <root@ubuntu-512mb-ams3-01.localdomain>
2016-12-11 12:11:48 +0000

    Cleaning up files

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,9 +3,3 @@ class ssh { }
 class ufw { }
 class wordpress { }
 
-#include lamp
-#include ssh
-#include ufw
-# include userpasi
-#include wordpress
-
--- modules/userpasi/manifests/init.pp
+++
@@ -1,7 +0,0 @@
-class userpasi {
-	user { 'ppesonen':
-		ensure => 'present',
-		managehome => true,
-		groups => ['sudo', 'adm'],
-	}
-}
